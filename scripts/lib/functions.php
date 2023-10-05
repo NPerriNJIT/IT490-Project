a173496cfd2b7fb8a89c85dce476753b3a74c97d
Nick Perri <nap3@njit.edu>
2023-10-05 13:38:50 -0400

    Added functions file, should make everything else more readable. Also added base login function, will have to do more work on this

---
+++ scripts/lib/functions.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+function doLogin($username, $password)
+{
+	$db = getDB();
+        $stmt = $db->prepare("SELECT id, username, password from Users where username = :username");
+        try {
+            $r = $stmt->execute([":username" => $username]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+			    return true;
+			    //we will have to pass things linked to users here, such as a team ID if we are sticking with fantasy football
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception $e) {
+            //TODO: Add error logging
+        }
+}
+
+?>
