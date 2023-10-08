ac10935c4a2233396aae8baaf12eb55f61ed77a1
Nick Perri <nap3@njit.edu>
2023-10-08 14:04:36 -0400

    Fixed users table

--- tables/create_table_users.sql
+++ tables/create_table_users.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS `Users` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`email` VARCHAR(100) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`username` VARCHAR(100) NOT NULL UNIQUE,
 	`password` VARCHAR(60) NOT NULL,
 	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`PRIMARY KEY (`id`),
-	UNIQUE (`email`)
+	`PRIMARY KEY (`id`)
 )
 /*This is the first table, it will be used to store basic user information.
 NOTE: I don't remember if this is going to be enough to securely store passwords, will look into it more.*/
