e61e9010c8b4177be146314005bb292cb6940558
Nick Perri <nap3@njit.edu>
2023-10-11 11:39:41 -0400

    Added requirements

--- loginForm.php
+++ loginForm.php
@@ -1,35 +1,37 @@
 <?php 
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
-    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
+$request = array();
+$request['type'] = "login";
+$request['username'] = $_POST['username'];
+$request['password'] = $_POST['password'];
 
-    $request = array();
-    $request['type'] = "login";
-    $request['username'] = $_POST['username'];
-    $request['password'] = $_POST['password'];
-
-    $response = $client->send_request($request);
+$response = $client->send_request($request);
 
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
+	<meta charset="UTF-8">
 
     </head>
 
     <body>
-        <form method="POST">
-            <label for="username"> Username: </label><br>
-            <input type="text" id="username" name="username"><br>
-            
-            <label for="password"> Password: </label><br>
-            <input type="text" id="password" name="password"><br>>
+	<form method="POST">
+	    <label for="username"> Username: </label><br>
+	    <input type="text" id="username" name="username"><br>
 
-            <button type="submit"> Login </button>
+	    <label for="password"> Password: </label><br>
+	    <input type="text" id="password" name="password"><br>>
 
-        </form>
+	    <button type="submit"> Login </button>
+
+	</form>
 
     </body>
 
