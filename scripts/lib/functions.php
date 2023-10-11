2198202dd715db78883616ce2598894aa8336a69
Nick Perri <nap3@njit.edu>
2023-10-11 11:34:10 -0400

    Added some debugging lines

--- scripts/lib/functions.php
+++ scripts/lib/functions.php
@@ -4,27 +4,29 @@ require_once("db.php");
 function doLogin($username, $password)
 {
 	$db = getDB();
-        $stmt = $db->prepare("SELECT id, username, password from Users where username = :username");
-        try {
-            $r = $stmt->execute([":username" => $username]);
-            if ($r) {
-                $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                if ($user) {
-                    $hash = $user["password"];
-                    unset($user["password"]);
-                    if (password_verify($password, $hash)) {
-			    return true;
-			    //we will have to pass things linked to users here, such as a team ID if we are sticking with fantasy football
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-            }
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
-        }
+	$stmt = $db->prepare("SELECT id, username, password from Users where username = :username");
+	try {
+		$r = $stmt->execute([":username" => $username]);
+		if ($r) {
+			$user = $stmt->fetch(PDO::FETCH_ASSOC);
+			if ($user) {
+				$hash = $user["password"];
+				unset($user["password"]);
+				if (password_verify($password, $hash)) {
+					echo($user + " logged in successfully");
+					return true;
+					//we will have to pass things linked to users here, such as a team ID if we are sticking with fantasy football
+				} else {
+					echo($user + " failed login attempt");
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	} catch (Exception $e) {
+		echo "Error: " . $e->getMessage();
+	}
 }
 
 ?>
