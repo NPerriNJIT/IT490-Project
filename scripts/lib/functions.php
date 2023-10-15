1ab9f7f425b71accc9d7086564fc761c81365cf3
Nick Perri <nap3@njit.edu>
2023-10-15 13:03:58 -0400

    Added session validation function, probably will need to be fixed

--- scripts/lib/functions.php
+++ scripts/lib/functions.php
@@ -14,7 +14,8 @@ function doLogin($username, $password)
 				unset($user["password"]);
 				if (password_verify($password, $hash)) {
 					echo($user + " logged in successfully");
-					return true;
+					//TODO: Create a session client-side with ID matching the session here
+					//TODO: Create a session here with username, other useful information
 					//we will have to pass things linked to users here, such as a team ID if we are sticking with fantasy football
 				} else {
 					echo($user + " failed login attempt");
@@ -35,12 +36,29 @@ function doRegistration($username, $password)
         $db = getDB();
         $stmt = $db->prepare("INSERT INTO Users (password, username) VALUES(:password, :username)");
         try {
-            $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
-            flash("You've registered, yay...");
+            $stmt->execute([":password" => $hash, ":username" => $username]);
+            echo $username . " registered successfully";
         } catch (Exception $e) {
             echo "Error: " . $e->getMessage();
         }
 
+}
+function doValidate($sessionID)
+{
+	$db = getDB();
+	$stmt = $db->prepare("Select data from Sessions where id = :sessionID");
+	try {
+		$r = $stmt->execute([":sessionID" = $sessionID]);
+		if($r) {
+			$session = $stmt->fetch(PDO::FETCH_ASSOC);
+			return $session['data'];
+		} else {
+			echo "Invalid session requested"
+			return null;
+		}
+	} catch (Exception $e) {
+		echo "Error: " . $e->getMessage();
+	}
 }
 
 ?>
