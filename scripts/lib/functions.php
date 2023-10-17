58fa211be0ea697480b799256367972d145a823b
Nick Perri <nap3@njit.edu>
2023-10-17 19:14:29 -0400

    added slashes for syntax

--- scripts/lib/functions.php
+++ scripts/lib/functions.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
-require_once(__DIR__ . "db.php");
-require_once(__DIR__ . '../path.inc');
-require_once(__DIR__ . '../get_host_info.inc');
-require_once(__DIR__ . '../rabbitMQLib.inc');
+require_once(__DIR__ . "/db.php");
+require_once(__DIR__ . '/../path.inc');
+require_once(__DIR__ . '/../get_host_info.inc');
+require_once(__DIR__ . '/../rabbitMQLib.inc');
 function doLogin($username, $password)
 {
 	echo "1";
