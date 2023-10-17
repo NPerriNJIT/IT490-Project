a980220390ae7e9678981209e262925a649a6bac
Nick Perri <nap3@njit.edu>
2023-10-17 11:51:17 -0400

    Added a page to test session data

---
+++ public_html/sessionTestPage.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../scripts/lib/functions.php");
+?>
+
+<html>
+    <head>
+	<meta charset="UTF-8">
+
+    </head>
+    <body>
+        <p>
+            Logged in: 
+            <?php if(is_logged_in()) : ?>
+                True 
+                Username: <?php echo(get_session_data(['username'])) ?>
+            <?php else : ?> 
+                False 
+            <?php endif ?>
+        </p>
+    </body>
+</html>
