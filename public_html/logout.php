a481c488e34cca292321faa736359484e09943ae
Nick Perri <nap3@njit.edu>
2023-10-24 14:18:12 -0400

    Fix for logout

--- public_html/logout.php
+++ public_html/logout.php
@@ -1,10 +1,5 @@
 <?php
 //Logs the user out if they were logged in, always redirects back to login
 require_once(__DIR__ . '/../scripts/lib/functions.php');
-if(is_logged_in()) {
-    reset_session();
-    flash("Successfully logged out", "success");
-} else {
-    flash("You never logged in...", "warning");
-}
+reset_session();
 die(header("Location: loginForm.php"));
