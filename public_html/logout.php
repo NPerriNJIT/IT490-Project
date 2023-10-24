2d25e51ccdc9561220c7a931b6c08c3b50d49644
Nick Perri <nap3@njit.edu>
2023-10-24 14:19:38 -0400

    new fix attempt for logout

--- public_html/logout.php
+++ public_html/logout.php
@@ -1,5 +1,5 @@
 <?php
 //Logs the user out if they were logged in, always redirects back to login
-require_once(__DIR__ . '/../scripts/lib/functions.php');
+require_once(__DIR__ . "/../scripts/partials/nav.php");
 reset_session();
 die(header("Location: loginForm.php"));
