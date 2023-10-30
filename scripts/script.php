04c61d6195ee2d399699cdf7cb45f3fe7d36c293
jheaney7 <jth5@njit.edu>
2023-10-30 17:17:29 -0400

    editing script.php again 7

--- scripts/script.php
+++ scripts/script.php
@@ -51,7 +51,7 @@ function populateDatabase($drink_id, $conn) {
         }
 
         // Bind the parameters
-        $stmt->bind_param("isssssssd", $drink_id, $drink['strDrink'], $drink['strTags'], $alcoholic_param, $ingredients_param, $measurements_param, $drink['strInstructions'], $avgrating_param);
+        $stmt->bind_param("issssssd", $drink_id, $drink['strDrink'], $drink['strTags'], $alcoholic_param, $ingredients_param, $measurements_param, $drink['strInstructions'], $avgrating_param);
 
         // Execute the prepared statement
         if ($stmt->execute()) {
@@ -65,12 +65,52 @@ function populateDatabase($drink_id, $conn) {
     }
 }
 
-// Start populating the database from a specified drink_id
 $drink_id = 11000;
+$waitTime = 10; // Wait time in seconds
+
 while (true) {
-    populateDatabase($drink_id, $conn);
+    if ($drink_id !== null) {
+        $response = file_get_contents("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" . $drink_id);
+
+        if ($response === false) {
+            $http_response_header_string = implode(",", $http_response_header);
+
+            // Check if the response contains a "429 Too Many Requests" error
+            if (strpos($http_response_header_string, "429 Too Many Requests") !== false) {
+                echo "Rate limit reached. Waiting for $waitTime seconds...\n";
+                sleep($waitTime);
+                continue; // Retry after waiting
+            }
+        }
+
+        // Parse the response
+        $data = json_decode($response, true);
+
+        if ($data['drinks'] != null) {
+            if (!isDuplicateDrink($drink_id, $conn)) {
+                populateDatabase($drink_id, $conn);
+            } else {
+                echo "Drink with ID $drink_id already exists. Skipping.\n";
+            }
+        }
+    }
     $drink_id++;
 }
+
+
+function isDuplicateDrink($drink_id, $conn) {
+    $query = "SELECT COUNT(*) FROM Drinks WHERE drink_id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $drink_id);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    
+    $stmt->fetch();
+    $stmt->close();
+    
+    return $count > 0;
+}
+
 
 // Close the database connection
 $conn->close();
