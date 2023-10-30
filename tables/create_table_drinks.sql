ff454a068880b889399e6dca288368aa9a88521e
jheaney7 <jth5@njit.edu>
2023-10-29 23:57:40 -0400

    edited drinks table

--- tables/create_table_drinks.sql
+++ tables/create_table_drinks.sql
@@ -2,20 +2,12 @@ CREATE TABLE IF NOT EXISTS `DRINKS` (
         `drink_id` INT PRIMARY KEY NOT NULL UNIQUE,
         `drink_name` VARCHAR(60) NOT NULL,
         `drink_tags` VARCHAR(255),
-        `alcoholic` BOOLEAN,
-        `ingredients` TEXT,
-        `measurements` TEXT,
-        `instructions` TEXT,
-        `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);CREATE TABLE IF NOT EXISTS `DRINKS` (
-        `drink_id` INT PRIMARY KEY NOT NULL UNIQUE,
-        `drink_name` VARCHAR(60) NOT NULL,
-        `drink_tags` VARCHAR(255) NOT NULL,
         `alcoholic` BOOLEAN NOT NULL,
         `ingredients` TEXT NOT NULL,
         `measurements` TEXT NOT NULL,
         `instructions` TEXT NOT NULL,
-        `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
-;
+        `avgrating` DOUBLE,
+        `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+);
 
