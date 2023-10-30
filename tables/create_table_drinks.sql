b30d79ee9ca44bd3e62588657ad0678a1b3ccadb
jheaney7 <jth5@njit.edu>
2023-10-29 20:36:03 -0400

    added drinks table

---
+++ tables/create_table_drinks.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `DRINKS` (
+        `drink_id` INT PRIMARY KEY NOT NULL UNIQUE,
+        `drink_name` VARCHAR(60) NOT NULL,
+        `drink_tags` VARCHAR(255) NOT NULL,
+        `alcoholic` BOOLEAN NOT NULL,
+        `ingredients` TEXT NOT NULL,
+        `measurements` TEXT NOT NULL,
+        `instructions` TEXT NOT NULL,
+        `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+;
+
