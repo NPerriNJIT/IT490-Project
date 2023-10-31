edb1617793144e8420b33fe716bdec7705620cf8
jheaney7 <jth5@njit.edu>
2023-10-31 14:41:33 -0400

    added rating form with a check

--- public_html/drink.php
+++ public_html/drink.php
@@ -12,4 +12,39 @@ $avg_rating = $get_reviews['average_rating'];
 $comments = array();
 $comments['text'] = $get_reviews['comments'];
 $comments['review_id'] = $get_reviews['review_id'];
+
+if (isset($_POST["submit"])) {
+    if (!isset($_POST["rating_number"]) || !isset($_POST["comment"])) {
+        flash("Please fill all fields", "danger");
+    }
+}  
+?>
+<h1>Add Rating</h1>
+<form method="POST">
+    <h3>Rating</h3>
+	<label for="1">☆</label>
+  	<input type="radio" id="1" name="rating_number" value="1">
+      <br>
+	<label for="2">☆☆</label>
+ 	<input type="radio" id="2" name="rating_number" value="2">
+    <br>
+  	<label for="3">☆☆☆</label>
+  	<input type="radio" id="3" name="rating_number" value="3">
+      <br>
+  	<label for="4">☆☆☆☆</label>
+  	<input type="radio" id="4" name="rating_number" value="4">
+      <br>
+    <label for="5">☆☆☆☆☆</label>
+  	<input type="radio" id="5" name="rating_number" value="5">
+      <br>
+<br>
+    <h4>Comments</h4>
+        <label for="comment"></label><br>
+        <input type="text" id="comment" name="comment"><br>
+        <input type="submit" value="Submit" name="submit">
+</form>
+</div>
+
+<?php
+require(__DIR__ . "/../scripts/partials/flash.php");
 ?>
