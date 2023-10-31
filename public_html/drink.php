006733d09af47f1916808c357a8302bba1b95659
Nick Perri <nap3@njit.edu>
2023-10-31 17:06:14 -0400

    Drink stuff

--- public_html/drink.php
+++ public_html/drink.php
@@ -8,11 +8,11 @@ if(!isset($_GET['id'])) {
 $drink_id = $_GET['id'];
 $drink = get_drink_info($drink_id);
 $get_reviews = get_drink_reviews($drink_id);
-print_r($get_reviews['average_rating']);
-$avg_rating = $get_reviews['average_rating'];
-$comments = array();
-$comments['text'] = $get_reviews['comments'];
-$comments['review_id'] = $get_reviews['review_id'];
+print_r($get_reviews);
+//$avg_rating = $get_reviews['average_rating'];
+//$comments = array();
+//$comments['text'] = $get_reviews['comments'];
+//$comments['review_id'] = $get_reviews['review_id'];
 
 if (isset($_POST["submit"])) {
     if (!isset($_POST["rating_number"]) || !isset($_POST["comment"])) {
