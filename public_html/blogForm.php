f8e381342f27b88088afc574f5b134530ca64e60
Nick Perri <nap3@njit.edu>
2023-10-31 11:28:25 -0400

    Added php functionality

--- public_html/blogForm.php
+++ public_html/blogForm.php
@@ -19,6 +19,12 @@ require_once(__DIR__ . "/../scripts/partials/nav.php");
         </form>
     </body>
 </html>
+<?php 
+if(isset($_POST['blogTitle']) && isset($_POST['blogContent'])) {
+    send_blog_post($_POST['blogTitle'], $_POST['blogContent']);
+    die(header("Location: blogForm.php"));
+}
+?>
 <?php
 require(__DIR__ . "/../scripts/partials/flash.php");
 ?>
