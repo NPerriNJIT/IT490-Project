20fa6298a06b50b9fd214eb7800228d0b6331366
Nick Perri <nap3@njit.edu>
2023-10-31 15:30:40 -0400

    Changed blog form

--- public_html/blogForm.php
+++ public_html/blogForm.php
@@ -8,14 +8,14 @@ require_once(__DIR__ . "/../scripts/partials/nav.php");
 
     </head>
     <body>
-        <form onsubmit="return send_blog_post(document.getElementById('blogTitle').value, document.getElementById('blogPost').value)" method="POST">
+        <form method="POST">
             <label for="blogTitle">Title:</label><br>
             <input type="text" id="blogTitle" name="blogTitle"><br><br>
 
             <label for="blogContent">Content:</label><br>
             <textarea id="blogContent" rows="4" cols="50"></textarea><br><br>
 
-            <button type="submit">
+            <button type="submit">Submit</button>
         </form>
     </body>
 </html>
