81129dca0f4d6cedc71a7f8f49322f2f9216083f
ZachGG421git config --global user.name ZachGG421git config --global user.name ZachGG421 <chubbizg@gmail.com>
2023-10-30 23:46:48 -0400

    added Drink.js class

---
+++ scripts/Drink.js
@@ -0,0 +1,14 @@
+class Drink {
+    constructor(id, name, alcoholic, ingredients, measurements, instructions, category, image, glass, tags) {
+        this.id = id;
+        this.name = name;
+        this.alcoholic = alcoholic;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.measurements = measurements;
+        this.category = category;
+        this.image = image;
+        this.glass = glass;
+        this.tags = tags;
+    }
+}
