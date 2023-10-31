32ad063d5067f5d8890da2a81b7c57519943ccd7
Nick Perri <nap3@njit.edu>
2023-10-31 16:08:26 -0400

    More debugging

--- public_html/blogForm.php
+++ public_html/blogForm.php
@@ -17,7 +17,7 @@ if(!is_logged_in()) {
             <input type="text" id="blogTitle" name="blogTitle"><br><br>
 
             <label for="blogContent">Content:</label><br>
-            <textarea id="blogContent" rows="4" cols="50"></textarea><br><br>
+            <textarea id="blogContent" name="blogContent" rows="4" cols="50"></textarea><br><br>
 
             <button type="submit">Submit</button>
         </form>
@@ -25,6 +25,7 @@ if(!is_logged_in()) {
 </html>
 <?php 
 if(isset($_POST['blogTitle']) && isset($_POST['blogContent'])) {
+    flash("test", "success");
     print_r(isset($_POST['blogTitle']) && isset($_POST['blogContent']));
     echo("Posting");
     //send_blog_post($_POST['blogTitle'], $_POST['blogContent']);
