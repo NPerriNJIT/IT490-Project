78c19c6ddbebbcd1027deec44f6bde8f1227475f
Nick Perri <nap3@njit.edu>
2023-10-31 22:59:12 -0400

    more redirect changes

--- public_html/profile.php
+++ public_html/profile.php
@@ -9,11 +9,12 @@ $redirect_id = get_session_user_id();
 if(isset($_GET['id'])) {
     $user_id = $_GET['id'];
 } else {
-    die(header("Location: profile?id=" . $redirect_id));
+    die(header("Location: profile.php?id=" . $redirect_id));
 }
 error_log("user id $user_id");
 if ($user_id < 1 || !check_user_exists($user_id)) {
     flash("Invalid user", "danger");
+    die(header("Location: profile.php?id=" . $redirect_id));
 }
 
 //TODO: Add functions to get profile info for user
