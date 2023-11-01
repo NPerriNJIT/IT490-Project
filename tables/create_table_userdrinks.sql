33fc9c7c213d339518b4ea6bf40d2857c10faa6f
Nick Perri <nap3@njit.edu>
2023-11-01 12:35:02 -0400

    fixed auto_increment

--- tables/create_table_userdrinks.sql
+++ tables/create_table_userdrinks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `UserDrinks` (
-        `drink_id` INT PRIMARY KEY NOT NULL UNIQUE,
+        `drink_id` INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
         `drink_name` VARCHAR(60) NOT NULL,
         `user_id` INT NOT NULL,
         `drink_tags` VARCHAR(255),
