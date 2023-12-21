05a351bfd590db57ed9ee526c34be56f4f4aff13
Nick Perri <nap3@njit.edu>
2023-12-20 20:51:31 -0500

    debug

--- scripts/lib/functions.php
+++ scripts/lib/functions.php
@@ -422,6 +422,8 @@ function get_user_activity () {
     $request['type'] = 'get_user_activity';
     $response = $client->send_request($request);
     if(isset($response['get_user_activity_status']) && $response['get_user_activity_status'] === 'valid') {
+		echo($response['user_activity']);
+		//^debug
 		return $response['user_activity'];
 	} else {
 		return [];
